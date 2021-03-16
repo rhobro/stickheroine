531ec58d68247bce88e90dd9caf1667bdf841bee
Rohan Mathew <robozmat@gmail.com>
2021-03-16 17:16:59 +0000

    update

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import io
+from time import time as t, sleep as s
 
 import numpy as np
 from PIL import Image
 from ppadb.client import Client as Adb
 
 ESTIMATE_Y = 2600
+CHERRY_Y = 2290
 REPLAY_COORD = (1150, 2130)
 STICK_GEN_SPEED = 1
-STICK_GEN_COEFF = 0.75
+STICK_GEN_COEFF = .7
 
 adb = Adb()
-device = adb.device("192.168.0.10:5555")
+device = adb.devices()[0]
 
 
 def main():
     while True:
-        p_row = screen()[ESTIMATE_Y].sum(axis=1)
+        scr = screen()
+        p_row = scr[ESTIMATE_Y].sum(axis=1)
 
         # find x-coord for gap start
         gap_start = 0
@@ -41,10 +44,39 @@ def main():
                     pillar_end = i
                     break
 
-        print(pillar_start, pillar_end, gap_start)
         stick_len = (pillar_start + pillar_end) // 2 - gap_start
-        device.input_swipe(500, 500, 500, 500, int(STICK_GEN_COEFF * stick_len * STICK_GEN_SPEED))
+        move_t = t() * 1000
+        device.input_swipe(500, 500, 500, 500, dist2time(stick_len))
+
+        # calculate cherry position
+        cherry_row = scr[CHERRY_Y, gap_start: pillar_start]
+        cherry_row_s = cherry_row.sum(axis=1)
+        cherry_dist = 0
+        for i in range(len(cherry_row_s[:-80])):
+            if 250 < cherry_row_s[i: i + 80].mean() < 260:
+                cherry_dist = i
+                break
+
+        # send flip commands
+        if cherry_dist != 0:
+            delay = dist2time(cherry_dist) - 100
+            while t() * 1000 < move_t + delay:
+                continue
+            print("now")
+            device.input_tap(500, 500)
+            s(.2)
+            device.input_tap(500, 500)
+
         wait_until_still()
+        # failed? restart?
+        btn_colours = screen()[REPLAY_COORD[1], REPLAY_COORD[0]]
+        if (btn_colours == (104, 104, 104)).all():
+            device.input_tap(REPLAY_COORD[0], REPLAY_COORD[1])
+            wait_until_still()
+
+
+def dist2time(pixels) -> int:
+    return int(STICK_GEN_COEFF * pixels * STICK_GEN_SPEED)
 
 
 def wait_until_still():
@@ -52,7 +84,7 @@ def wait_until_still():
         continue
 
 
-def screen():
+def screen() -> np.ndarray:
     return np.array(Image.open(io.BytesIO(device.screencap())))[:, :, :3]
 
 
